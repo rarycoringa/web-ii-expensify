2f0a669f5aaf701fe855daff31d8d8ec45afc7f7
Rary Coringa <rary.goncalves.123@ufrn.edu.br>
2026-02-03 14:44:57 -0300

    fix cors settings to allow receive requests from frontend app

--- src/main/java/br/edu/ufrn/expensify/config/SecurityConfig.java
+++ src/main/java/br/edu/ufrn/expensify/config/SecurityConfig.java
@@ -1,9 +1,13 @@
 package br.edu.ufrn.expensify.config;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -11,6 +15,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
@@ -35,7 +42,7 @@ public class SecurityConfig {
             throws Exception {
 
         http
-            .cors(cors -> {})
+            .cors(Customizer.withDefaults())
             .csrf(csrf -> csrf.disable())
             .formLogin(form -> form.disable())
             .httpBasic(basic -> basic.disable())
@@ -44,6 +51,7 @@ public class SecurityConfig {
             )
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(HttpMethod.POST, "/auth/**").permitAll()
+                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .anyRequest().authenticated()
             )
             .addFilterBefore(
@@ -53,5 +61,22 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    @Bean
+	public CorsConfigurationSource corsConfigurationSource(
+			@Value("${EXPENSIFY_APP_URL:http://localhost:3000}") String expensifyAppUrl
+	) {
+		CorsConfiguration config = new CorsConfiguration();
+
+		config.setAllowedOrigins(List.of(expensifyAppUrl));
+		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		config.setAllowedHeaders(List.of("*"));
+		config.setAllowCredentials(true);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+
+		return source;
+	}
 
 }
